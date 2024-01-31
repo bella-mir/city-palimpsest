0788c10b1f288d21184e2487bad4179d5658c712
Bella Mironova <bellamironova@gmail.com>
2024-01-31 23:08:24 +0100

    add legend style based on a selection

--- src/components/map/components/legend/legend-constants.ts
+++ src/components/map/components/legend/legend-constants.ts
@@ -1,13 +1,13 @@
 import { DefaultOptionType } from "antd/es/select";
 
 export const LEGEND_COLORS: { [key: string]: string } = {
-  "Market Site": "#d99e32",
-  "Royal Residence": "#f27649",
-  "A New Munich": "#668c54",
-  Metropolis: "#bfba73",
-  "World Wars": "#312640",
-  "Post War": "#8C7D4F",
-  "Our Days": "#568FA6",
+  "Market Site (before 1730s)": "#d99e32",
+  "Royal Residence (~1740s-1840s)": "#f27649",
+  "A New Munich (~1850s-1880s)": "#668c54",
+  "Metropolis (~1890s-1900s)": "#bfba73",
+  "World Wars (~1910s-1940s)": "#312640",
+  "Post War (~1950s-1990s)": "#8C7D4F",
+  "Our Days (~2000+)": "#568FA6",
 };
 
 export const CATEGORIES: DefaultOptionType[] = [
@@ -18,5 +18,5 @@ export const CATEGORIES: DefaultOptionType[] = [
   { value: 4, label: "Metropolis" },
   { value: 5, label: "World Wars" },
   { value: 6, label: "Post War" },
-  { value: 6, label: "Our Days" },
+  { value: 7, label: "Our Days" },
 ];
--- src/components/map/components/legend/legend.tsx
+++ src/components/map/components/legend/legend.tsx
@@ -4,9 +4,20 @@ import styles from "./legend.module.scss";
 import Select from "antd/es/select";
 import { useAppDispatch } from "../../../../app/app-types";
 import { setFilter } from "../../../../app/app-actions";
+import { useSelector } from "react-redux";
+import { getFilter } from "../../../../app/app-selectors";
 
 export const Legend = () => {
   const dispatch = useAppDispatch();
+  const filter = useSelector(getFilter);
+
+  const filterClass = (index: number) => {
+    if (filter === "all") {
+      return true;
+    } else {
+      return index === filter - 1;
+    }
+  };
 
   const onChange = (value: "all" | 1 | 2 | 3 | 4 | 5 | 6 | 7) => {
     dispatch(setFilter(value));
@@ -23,16 +34,24 @@ export const Legend = () => {
         className={styles.selector}
         onChange={onChange}
       />
-      {Object.keys(LEGEND_COLORS).map((key) => (
+      {Object.keys(LEGEND_COLORS).map((key, index) => (
         <div key={key} className={styles.row}>
           <div
             className={styles.cube}
             style={{
               backgroundColor: `${LEGEND_COLORS[key]}`,
               borderColor: LEGEND_COLORS[key],
+              opacity: filterClass(index) ? 1 : 0.3,
             }}
           ></div>
-          <span>{key}</span>
+          <span
+            className={classNames({ [styles.selected]: filterClass(index) })}
+            style={{
+              opacity: filterClass(index) ? 1 : 0.5,
+            }}
+          >
+            {key}
+          </span>
         </div>
       ))}
     </div>
